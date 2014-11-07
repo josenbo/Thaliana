05ab202f8a184966367b150317b117f2ce9577f2
josenbo <github1405js@anxi.de>
2014-11-07 19:28:40 +0100

    nicht mehr benötigte Teilprojekte entfernt

--- BongDownloadConsole/BongRecordings.Designer.cs
+++
@@ -1,4 +0,0 @@
-﻿// Die standardmäßige Codegenerierung für Modell 'D:\Entwicklung\2014\Thaliana\Thaliana-Git\BongDownloadConsole\BongRecordings.edmx' ist deaktiviert. 
-// Ändern Sie zur Aktivierung der standardmäßigen Codegenerierung den Wert der Designer-Eigenschaft 'Codegenerierungsstrategie'
-// in einen anderen Wert. Diese Eigenschaft ist im Fenster 'Eigenschaften' verfügbar, wenn das Modell
-// im Designer geöffnet ist.
